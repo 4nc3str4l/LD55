462c0a659ab4e220ad2fa9437c492571d84ea6c8
4nc3str4l <cmuri_10@hotmail.com>
2024-04-14 20:34:05 +0200

    Level 3 tutorial

--- src/InGameScene.cpp
+++ src/InGameScene.cpp
@@ -239,7 +239,7 @@ World *InGameScene::GetWorld(int level)
 void InGameScene::Load()
 {
     gameState = GameState::STARTING;
-    currentLevel = 2;
+    currentLevel = 3;
 
     RegisterWorld(1);
     RegisterWorld(2);
