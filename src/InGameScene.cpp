2bd6c2489d323481f3d61e8c18ecb43f1fa97a56
4nc3str4l <cmuri_10@hotmail.com>
2024-04-14 19:10:26 +0200

    Level 10

--- src/InGameScene.cpp
+++ src/InGameScene.cpp
@@ -239,7 +239,7 @@ World *InGameScene::GetWorld(int level)
 void InGameScene::Load()
 {
     gameState = GameState::STARTING;
-    currentLevel = 9;
+    currentLevel = 10;
 
     RegisterWorld(1);
     RegisterWorld(2);
@@ -250,6 +250,8 @@ void InGameScene::Load()
     RegisterWorld(7);
     RegisterWorld(8);
     RegisterWorld(9);
+    RegisterWorld(10);
+    
 
     this->world = GetWorld(currentLevel);
 
