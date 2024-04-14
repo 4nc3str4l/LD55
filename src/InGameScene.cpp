9a6b977c473d2f37a3a33c5c6735652f78db8a14
4nc3str4l <cmuri_10@hotmail.com>
2024-04-14 20:28:39 +0200

    Fix level 2 tutorial

--- src/InGameScene.cpp
+++ src/InGameScene.cpp
@@ -239,7 +239,7 @@ World *InGameScene::GetWorld(int level)
 void InGameScene::Load()
 {
     gameState = GameState::STARTING;
-    currentLevel = 1;
+    currentLevel = 2;
 
     RegisterWorld(1);
     RegisterWorld(2);
